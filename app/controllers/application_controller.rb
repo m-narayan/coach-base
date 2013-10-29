ec40975bce1b2082daa4e656007d55e2112a5e0a
natarajan <saranjasminenv@gmail.com>
2013-10-29 14:15:48 +0530

    manage-course

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-
+  require 'will_paginate'
   def after_sign_in_path_for(resource_or_scope)
     if redirect_back_req?
       redirect_back
