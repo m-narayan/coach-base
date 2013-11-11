6bcd2447c4980f81ddc1033391bb238177880f45
sukumar <sukumarreddy@arrivusystems.com>
2013-11-11 11:02:46 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoachBase::Application.config.secret_token = '01ca4c77392ce695baaa25b8f94752126449284f9a11efec8398e0d6f514ab5225522519168149b41bfbd5ed14cb4e50e1c56dfb696197a005e9aac635aee87d'
